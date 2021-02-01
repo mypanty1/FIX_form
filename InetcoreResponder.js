4fad8d17d3a64e2b0edebdec5f5596ca27b178f5
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2021-02-01 18:58:33 +0700

    Add files via upload

--- InetcoreResponder.js
+++ InetcoreResponder.js
@@ -1,6 +1,6 @@
 javascript:(function(){if(document.title!='InetcoreResponder'&&(window.location.href.includes('https://fx.mts.ru')||window.location.href.includes('http://inetcore.mts.ru/fix')||window.location.href.includes('http://pre.inetcore.mts.ru/fix'))){
 	document.title='InetcoreResponder';
-	/*1818*/
+	/*1856*/
 	/*app communication*/
 	let timeout=1000;/*01sec*/
 	let lastStr='';
@@ -22,26 +22,41 @@ javascript:(function(){if(document.title!='InetcoreResponder'&&(window.location.
 			jsonObj=JSON.parse(str);
 			if(jsonObj.uid&&jsonObj.to_inetcore){
 				switch(jsonObj.to_inetcore.type){
-					case'echo':/*"{"to_inetcore":{"type":"echo"},"uid":"uid-1612004586606"}"*/
-						uidData[jsonObj.uid]=jsonObj.to_inetcore.type;
-						sendStr('data','echo_ok',jsonObj.uid);
+					/*проверка на доступность респондера*/
+					case'echo_responder':/*"{"to_inetcore":{"type":"echo_responder"},"uid":"uid-1612004586606"}"*/
+						uidData[jsonObj.uid]='responder_ok';
+						sendStr('data','responder_ok',jsonObj.uid);
 					break;
+					/*проверка на доступность методов инеткора*/
+					case'echo_inetcore':/*"{"to_inetcore":{"type":"echo_inetcore"},"uid":"uid-1612004586606"}"*/
+						if(typeof httpGet=='function'&&typeof httpPost=='function'){
+							uidData[jsonObj.uid]='inetcore_ok';
+							sendStr('data','inetcore_ok',jsonObj.uid);
+						}else{
+							uidData[jsonObj.uid]=false;
+							sendStr('data',false,jsonObj.uid);
+						};
+					break;
+					/*get запрос в инеткор*/
 					case'get':/*"{"to_inetcore":{"type":"get","url":"/call/main/get_user_data","prm":null},"uid":"uid-1612004586606"}"*/
 						httpGet(jsonObj.to_inetcore.url,true).then(function(data){
 							uidData[jsonObj.uid]=data;/*console.log(JSON.stringify(uidData[jsonObj.uid]));*/
 							sendStr('data',data,jsonObj.uid);
 						});
 					break;
+					/*post запрос в инеткор*/
 					case'post':/*"{"to_inetcore":{"type":"post","url":"/call/main/get_user_data","prm":null},"uid":"uid-1612004586606"}"*/
 						httpPost(jsonObj.to_inetcore.url,jsonObj.to_inetcore.prm,true).then(function(data){
 							uidData[jsonObj.uid]=data;/*console.log(JSON.stringify(uidData[jsonObj.uid]));*/
 							sendStr('data',data,jsonObj.uid);
 						});
 					break;
+					/*запрос недошедшего сообщения от респондера*/
 					case'uid_miss':/*"{"to_inetcore":{"type":"uid_miss","uid_miss":"uid-1612004586111"},"uid":"uid-1612004586222"}"*/
 						/*sendStr('data',uidData[jsonObj.to_inetcore.uid_miss],jsonObj.uid);*/
 						sendStr('data',uidData[jsonObj.to_inetcore.uid_miss],jsonObj.to_inetcore.uid_miss);
 					break;
+					/*сообщени от респондера принято, удалить из кэша*/
 					case'uid_ok':/*"{"to_inetcore":{"type":"uid_ok","uid_ok":"uid-1612004586111"},"uid":"no_uid"}"*/
 						uidData[jsonObj.to_inetcore.uid_ok]=false;
 					break;
