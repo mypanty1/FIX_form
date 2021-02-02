853dfbdb2bbb715c128def25cc64d1034225e7ff
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2021-02-02 17:05:09 +0700

    Delete InetcoreResponder_insertJS.js

--- InetcoreResponder_insertJS.js
+++
@@ -1,6 +0,0 @@
-javascript:(function(){if(document.title!='Inetcore_'&&document.title!='InetcoreResponder'&&(window.location.href.includes('https://fx.mts.ru')||window.location.href.includes('http://inetcore.mts.ru/fix')||window.location.href.includes('http://pre.inetcore.mts.ru/fix'))){
-	document.title='Inetcore_';
-	let addJS=document.createElement('script');
-	addJS.src='https://mypanty1.github.io/FIX_form/InetcoreResponder.js';
-	document.body.appendChild(addJS);
-}}());
